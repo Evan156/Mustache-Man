3bc00fbf7e73b3919c182b99891993fc1ccffea6
Evan156 <87071935+Evan156@users.noreply.github.com>
2021-10-05 16:32:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload(){
+
+
+}
+
+function setup(){
+canvas = createCanvas(500,500)
+canvas.center()
+video = createCapture(VIDEO);
+video.size(500,500)
+video.hide()
+
+}
+
+function draw(){
+image(video,0,0,500,500)
+
+}
+
+function Take_it(){
+save("Mr.Mustache")
+
+
+}
